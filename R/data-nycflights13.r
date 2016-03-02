211256efaf9ac142cdfaee055896b6b0e086a226
Christoph Hösler <christoph.hoesler@gmail.com>
2016-03-02 12:02:28 +0100

    Creating nyflights db as a file, not in memory

--- R/data-nycflights13.r
+++ R/data-nycflights13.r
@@ -9,28 +9,50 @@
 NULL
 
 #' @export
+#' @param use_cache Use cached database if present.
+#' @param clear_cache Remove database from cache if present.
 #' @rdname h2-nycflights13
-nycflights13_h2 <- function() {
-  cache_load("nycflights_h2", {
-    src <- src_h2("mem:nycflights")
-    dplyr::copy_nycflights13(src)
-  })
+nycflights13_h2 <- function(use_cache = TRUE, clear_cache = FALSE) {
+  if (use_cache) {
+    cache_load("nycflights_h2", {
+      nycflights_h2_create()
+    }, clear_cache)
+  } else {
+    nycflights_h2_create()
+  }
+}
+
+nycflights_h2_create <- function() {
+  url <- getOption("nycflights.h2.url")
+  if (is.null(url)) {
+    url <- paste0(tempfile("nycflights"), "/nycflights")
+  }
+  src <- src_h2(url)
+  message("Caching nycflights db at ", url)
+  dplyr::copy_nycflights13(src)
 }
 
 # Environment for caching
 cache <- new.env(parent = emptyenv())
 
 is_cached <- function(name) exists(name, envir = cache)
+
 cache_insert <- function(name, value) {
   assign(name, value, envir = cache)
-  value
 }
+
 cache_get <- function(name) {
   get(name, envir = cache)
 }
 
-cache_load <- function(name, computation) {
+cache_rm <- function(name) {
   if (is_cached(name)) {
+    rm(name, envir = cache)
+  }
+}
+
+cache_load <- function(name, computation, force = FALSE) { 
+  if (!force && is_cached(name)) {
     cache_get(name)
   } else {
     res <- force(computation)
--- README.md
+++ README.md
@@ -7,14 +7,14 @@ A [dplyr](https://github.com/hadley/dplyr) extension which allows to use a H2 da
 ## Installation
 Assuming [devtools](https://github.com/hadley/devtools) is installed, run:
 ```R
-devtools::install_github("hoesler/RJDBC")
-devtools::install_github("hoesler/RH2")
+devtools::install_github("hoesler/dbj")
+devtools::install_github("hoesler/dbj-h2")
 devtools::install_github("hoesler/dplyr-src-h2")
 ```
 
 ## Usage
 ```R
 src <- src_h2("mem:") # Use a URL (http://h2database.com/html/features.html#database_url)
-src <- src_h2(dbConnect(H2(), "mem:", "sa", "")) # Use a H2Connection
+src <- src_h2(dbConnect(dbj.h2::driver(), "mem:", "sa", "")) # Use a H2Connection
 src <- nycflights13_h2() # A H2 src with nycflights13 data (Used for tests, examples, ...)
 ```
