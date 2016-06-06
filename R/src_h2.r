d73294f21bdf6d4cb8864ce7dd97be9f7d7d5b6d
Christoph Hösler <christoph.hoesler@gmail.com>
2016-06-06 20:32:52 +0200

    Use dbj.h2

--- R/src_h2.r
+++ R/src_h2.r
@@ -1,22 +1,13 @@
 #' @importFrom DBI dbWriteTable dbGetQuery dbConnect dbGetInfo
-#' @importFrom dbj resolve module
 #' @import dplyr
 #' @importFrom methods new
-NULL
-
-#' A wrapper class for JDBCConnection objects created in src_h2. Used for H2 specific dispatch.
-#' @export
-setClass("H2JDBCConnection", contains = c("JDBCConnection"))
-
-drv_h2 <- function() {
-  dbj::driver('org.h2.Driver', resolve(module('com.h2database:h2:1.3.176')))
-}               
+NULL           
 
 #' Create a source object for an H2 database.
 #' 
-#' @param x an URL for the H2 database connection as defined at \url{http://h2database.com/html/features.html#database_url}
-#'  without the 'jdbc:h2:' prefix.
+#' @param x an URL for the H2 database connection as defined at \url{http://h2database.com/html/features.html#database_url}.
 #'  If \code{x} is a path to a local file, than the '.h2.db' suffix, if present, is stripped off automatically.
+#' @param drv_h2 The database driver
 #' @param src a h2 src created with \code{src_h2}.
 #' @param from Either a string giving the name of table in database, or
 #'   \code{\link{sql}} described a derived table or compound join.
@@ -28,21 +19,16 @@ src_h2 <- function(x, ...) UseMethod("src_h2")
 
 #' @export
 #' @describeIn src_h2 Create the source from an H2 URL.
-src_h2.character <- function(x, ...) {
-  url <- sprintf("jdbc:h2:%s", sub("^(.*)\\.h2\\.db$", "\\1", x))
-  dbj_connection <- dbConnect(drv_h2(), url, ...)
+src_h2.character <- function(x, drv_h2 = dbj.h2::driver(), ...) {
+  dbj_connection <- dbConnect(drv_h2, x, ...)
   src_h2(dbj_connection, ...)
 }
 
 #' @export
 #' @describeIn src_h2 Create the source from an JDBC connection.
-src_h2.JDBCConnection <- function(x, ...) {
+src_h2.H2Connection <- function(x, ...) {
   info <- dbGetInfo(x)
-  if (all(startsWith(info$url, "jdbc:h2"))) {
-    warning("Expected a jdbc:h2 URL. Is this a valid H2 connection?")
-  }
-  h2_connection <- new("H2JDBCConnection", x)
-  src_sql("h2", h2_connection, info = info, ...)
+  src_sql("h2", x, info = info, ...)
 }
 
 #' @export
@@ -77,13 +63,13 @@ src_translate_env.src_h2 <- function(x) {
 # DBI methods ------------------------------------------------------------------
 
 #' @export
-db_insert_into.H2JDBCConnection <- function(con, table, values, ...) {
+db_insert_into.H2Connection <- function(con, table, values, ...) {
   dbWriteTable(con, table, values, append = TRUE)
   invisible()
 }
 
 #' @export
-db_analyze.H2JDBCConnection <- function(con, table, ...) {
+db_analyze.H2Connection <- function(con, table, ...) {
   sql <- build_sql("ANALYZE", con = con)
   dbGetQuery(con, sql)
 }
